b39f78b3aa1578737c1cbdfb8f9a3dbb1290817a
Aiden Johnson <arjohnsonn12@gmail.com>
2025-04-02 04:05:43 -0500

    feat: share feature on articles

--- components/Interaction.tsx
+++ components/Interaction.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, Share } from 'react-native';
 
 type Props = {
   author: string;
+  url: string;
   authorImage?: string;
 };
 
+const TRUNCATE_LENGTH = 24;
+
 const Interaction = (props: Props) => {
+  const onShare = async () => {
+    try {
+      await Share.share({
+        message: 'Check out this article I found on StepUp!',
+        url: props.url,
+        title: 'Check out this article',
+      });
+    } catch (error) {
+      console.error('Error sharing article:', error);
+    }
+  };
+
   return (
     <View className="my-3 flex w-full flex-row justify-between">
-      <View className="flex flex-row items-center gap-x-3">
-        <View className="rounded-full bg-[#DCEFEF] p-1">
+      <View className="flex flex-row items-center gap-x-2">
+        <View className="rounded-full bg-[#DCEFEF] p-2">
           <Image
-            source={props.authorImage ?? require('../images/Profile.png')}
-            className="h-5 w-5"
+            source={props.authorImage ?? require('../images/profile.png')}
+            className="h-3 w-3"
           />
         </View>
-        <Text className="text-left text-lg font-semibold">{props.author}</Text>
+        <Text className="text-md text-left font-serif font-semibold">
+          {props.author.length > TRUNCATE_LENGTH
+            ? `${props.author.substring(0, TRUNCATE_LENGTH)}...`
+            : props.author}
+        </Text>
       </View>
       <View className="flex flex-row items-center gap-x-2">
         <TouchableOpacity>
@@ -24,7 +43,7 @@ const Interaction = (props: Props) => {
             <Image source={require('../images/save.png')} className="h-4 w-4" />
           </View>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={onShare}>
           <View className="rounded-full bg-[#DCEFEF] p-2">
             <Image source={require('../images/share.png')} className="h-4 w-4" />
           </View>
