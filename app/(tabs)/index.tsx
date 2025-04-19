36eba909aa7113a2bc702f9b238d99a8b1ff9a5f
Aiden Johnson <arjohnsonn12@gmail.com>
2025-04-19 17:35:04 -0500

    fix: step up button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,60 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { FlatList, ActivityIndicator, ListRenderItemInfo, View, Text } from 'react-native';
+import {
+  FlatList,
+  ActivityIndicator,
+  ListRenderItemInfo,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import { getTopArticles } from '~/lib/newsApi';
 import Article from '~/components/Article';
 import { ArticleData } from '~/store/articleStore';
 import { useArticleStore } from '~/store/articleStore';
+import { Sheet } from '~/components/nativewindui/Sheet';
+import { ScrollView } from 'react-native';
+import { getBulletedList } from '~/lib/gpt';
+import { useSheetRef } from '~/components/nativewindui/Sheet';
 
 const Screen: React.FC = () => {
   const [articles, setArticles] = useState<ArticleData[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [currentArticle, setCurrentArticle] = useState<ArticleData | null>(null);
+
+  const [stepUpText, setStepUpText] = useState<string | null>(null);
+
+  const bottomSheetModalRef = useSheetRef();
+
+  // Cache so we don't call the API every time
+  let cachedTitle: string;
+
+  async function stepUp() {
+    const currentArticle = useArticleStore.getState().currentArticle;
+    if (!currentArticle) {
+      console.warn('No current article found');
+      return;
+    }
+
+    setStepUpText('');
+
+    bottomSheetModalRef.current?.present();
+
+    try {
+      let text;
+      if (currentArticle.headline != cachedTitle) {
+        text = await getBulletedList(currentArticle?.summary);
+        cachedTitle = currentArticle.headline;
+      } else {
+        text = stepUpText;
+      }
+
+      setStepUpText(text);
+    } catch (error) {
+      console.error('Error fetching action items:', error);
+      setStepUpText('Failed to load action items. Please try again.');
+    }
+  }
 
   const fetchArticles = async () => {
     setLoading(true);
@@ -66,7 +112,7 @@ const Screen: React.FC = () => {
     </View>
   ) : (
     <>
-      <Text className="my-3 px-5 mt-16 font-serif text-2xl font-bold">Trending For You</Text>
+      <Text className="my-3 mt-16 px-5 font-serif text-2xl font-bold">Trending For You</Text>
 
       <FlatList
         data={articles}
@@ -79,6 +125,29 @@ const Screen: React.FC = () => {
         onViewableItemsChanged={updateCurrentArticle}
         viewabilityConfig={viewabilityConfig}
       />
+
+      <TouchableOpacity
+        onPress={stepUp}
+        className="mx-auto mb-3 w-10/12 items-center rounded-full bg-red-600 py-4">
+        <Text className="text-md font-bold text-white">Step Up</Text>
+      </TouchableOpacity>
+
+      <Sheet ref={bottomSheetModalRef} snapPoints={[stepUpText ? stepUpText.length / 1.1 : 350]}>
+        <View>
+          <Text className="text-center text-2xl font-black text-[#E32722]">Step Up!</Text>
+          {stepUpText != '' ? (
+            <ScrollView>
+              <Text className="text-md p-3 text-center leading-4">
+                {stepUpText?.replace(/\n/g, '\n\n')}
+              </Text>
+            </ScrollView>
+          ) : (
+            <View className="h-full w-full items-center justify-center">
+              <ActivityIndicator size="small" />
+            </View>
+          )}
+        </View>
+      </Sheet>
     </>
   );
 };
